b305db66eebd05c0437d5f26cd3632df8a90fc22
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2018-09-03 19:28:21 +0200

    define results route

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import Components from './components/components';
 import HomeComponent from './home/home.component';
+import ResultsComponent from './results/results.component';
 import {
   CheapFlightService,
   AirportsService
@@ -12,14 +13,20 @@ angular.module('myApp', [
   Components
 ])
 .component('homePage', HomeComponent)
+.component('resultsPage', ResultsComponent)
 .service('AiportsService', AirportsService)
 .service('CheapFlightService', CheapFlightService)
-.config(($stateProvider) => {
+.config(($stateProvider, $urlRouterProvider) => {
   'ngInject';
 
+  $urlRouterProvider.otherwise('/');
   $stateProvider
     .state('home', {
-      url: '',
+      url: '/',
       template: '<home-page></home-page>'
+    })
+    .state('results', {
+      url: '/results',
+      template: '<results-page></results-page>'
     });
 });
---
+++ src/results/results.component.js
@@ -0,0 +1,8 @@
+import template from './results.component.html';
+import './results.component.scss';
+
+const ResultsComponent = {
+  template
+};
+
+export default ResultsComponent;
