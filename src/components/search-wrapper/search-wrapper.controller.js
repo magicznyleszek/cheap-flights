5e5a93788409221d866b8a8ee191c3b3fc835c00
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2018-09-05 11:19:23 +0200

    start styling search wrapper

--- src/components/search-wrapper/search-wrapper.controller.js
+++ src/components/search-wrapper/search-wrapper.controller.js
@@ -4,6 +4,15 @@ export default function AirportSelectorController() {
     { label: 'Warsaw', id: 'waw' }
   ];
   this.destinationAirports = [
+    { label: 'Airport1', id: 'ai1' },
+    { label: 'Airport2', id: 'ai2' },
+    { label: 'Airport3', id: 'ai3' },
+    { label: 'Airport4', id: 'ai4' },
+    { label: 'Airport5', id: 'ai5' },
+    { label: 'Airport6', id: 'ai6' },
+    { label: 'Airport7', id: 'ai7' },
+    { label: 'Airport8', id: 'ai8' },
+    { label: 'Airport9', id: 'ai9' },
     { label: 'Paris', id: 'prs' },
     { label: 'London', id: 'lnd' }
   ];
