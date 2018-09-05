3fdebaac8607d8206cd15c3193a2259df7103d5d
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2018-09-05 18:43:26 +0200

    start working on airports service

--- src/services/airports.service.js
+++ src/services/airports.service.js
@@ -1,5 +1,7 @@
-const AirportsService = () => {
-  //TODO: Write your own implementation
-};
+export default function AirportsService($http) {
+  'ngInject';
 
-export default AirportsService;
+  this.apiURL = 'https://murmuring-ocean-10826.herokuapp.com/en/api/2/forms/flight-booking-selector/';
+
+  this.getAirports = () => $http({ method: 'get', url: this.apiURL, cache: true });
+}
