38f9a1fb6bac4e062a663165ea08a3cdd8947615
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2018-09-05 22:05:48 +0200

    define main-menu component

--- src/components/components.js
+++ src/components/components.js
@@ -1,14 +1,16 @@
 import angular from 'angular';
 import AirportSelectorComponent from './airport-selector/airport-selector.component';
 import DateSelectorComponent from './date-selector/date-selector.component';
-import SearchWrapperComponent from './search-wrapper/search-wrapper.component';
-import FlightsListComponent from './flights-list/flights-list.component';
 import FlightComponent from './flight/flight.component';
+import FlightsListComponent from './flights-list/flights-list.component';
+import MainMenuComponent from './main-menu/main-menu.component';
+import SearchWrapperComponent from './search-wrapper/search-wrapper.component';
 
 export default angular.module('app.components', [])
 .component('airportSelector', AirportSelectorComponent)
 .component('dateSelector', DateSelectorComponent)
-.component('searchWrapper', SearchWrapperComponent)
-.component('flightsList', FlightsListComponent)
 .component('flight', FlightComponent)
+.component('flightsList', FlightsListComponent)
+.component('mainMenu', MainMenuComponent)
+.component('searchWrapper', SearchWrapperComponent)
 .name;
---
+++ src/components/main-menu/main-menu.component.js
@@ -0,0 +1,10 @@
+import template from './main-menu.component.html';
+import controller from './main-menu.controller';
+import './main-menu.component.scss';
+
+const MainMenuComponent = {
+  template,
+  controller
+};
+
+export default MainMenuComponent;
