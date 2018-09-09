a913b530cc8e3a2f3d66eb098641b9e7a54b6c7c
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2018-09-09 14:15:52 +0200

    cheap flights tests

--- src/services/cheap-flights.service.js
+++ src/services/cheap-flights.service.js
@@ -1,7 +1,7 @@
 export default function CheapFlightsService($q, $http) {
   'ngInject';
 
-  const baseURL = 'https://murmuring-ocean-10826.herokuapp.com/en/api/2/flights/from/DUB/to/STN/2014-12-02/2015-02-02/250/unique/?limit=15&offset-0';
+  const baseURL = 'https://murmuring-ocean-10826.herokuapp.com/en/api/2/flights';
   const pageSize = 15;
 
   this.getURL = (sourceIataCode, destinationIataCode, startDate, endDate, page = 0) => {
--- src/services/cheap-flights.spec.js
+++ src/services/cheap-flights.spec.js
@@ -2,18 +2,42 @@ import CheapFlightsService from './cheap-flights.service';
 
 describe('CheapFlightsService', () => {
   let $httpBackend;
+  let $rootScope;
+  let cheapFlights;
 
   beforeEach(() => {
     angular.module('cheapFlightsApp', ['ng', 'ngMock'])
-      .component('cheapFlightsService', CheapFlightsService);
+      .service('CheapFlightsService', CheapFlightsService);
     angular.mock.module('cheapFlightsApp');
   });
 
   beforeEach(inject(($injector) => {
     $httpBackend = $injector.get('$httpBackend');
+    $rootScope = $injector.get('$rootScope');
+    cheapFlights = $injector.get('CheapFlightsService');
   }));
 
+  afterEach(() => {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+    $httpBackend.resetExpectations();
+  });
+
   it('should return flights array with findFlights method', () => {
-    expect(false).to.equal(true);
+    const testUrl = cheapFlights.getURL('a', 'b', 'c', 'd');
+
+    $httpBackend.expectGET(testUrl).respond(200, { data: { flights: [] } });
+
+    const foo = { successCallback: angular.noop };
+    sinon.spy(foo, 'successCallback');
+
+    cheapFlights.findFlights('a', 'b', 'c', 'd').then(
+      foo.successCallback
+    );
+
+    $httpBackend.flush();
+    $rootScope.$digest();
+
+    expect(foo.successCallback.called).to.equal(true);
   });
 });
