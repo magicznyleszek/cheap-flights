1a3de4ffe53ce99418331446a09ec40cb605b88c
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2018-09-09 11:44:03 +0200

    add sinon to be able to test spies

--- karma.conf.js
+++ karma.conf.js
@@ -5,10 +5,11 @@ module.exports = function (config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai'],
+    frameworks: ['mocha', 'chai', 'sinon'],
 
     plugins: [
       'karma-chai',
+      'karma-sinon',
       'karma-chrome-launcher',
       'karma-mocha',
       'karma-mocha-reporter',
