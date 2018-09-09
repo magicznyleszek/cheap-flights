405a994edc623ea338dc8bf08e15ada9630e3deb
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2018-09-09 13:27:36 +0200

    flights list tests

--- src/components/flights-list/flights-list.spec.js
+++ src/components/flights-list/flights-list.spec.js
@@ -5,6 +5,21 @@ describe('Component flightsList', () => {
   let scope;
   let componentEl;
 
+  const mockFlights = [
+    {
+      dateFrom: '2013-01-31T11:18:16.564Z',
+      dateTo: '2013-01-31T16:43:51.822Z',
+      currency: '€',
+      price: 100
+    },
+    {
+      dateFrom: '2015-01-31T11:18:16.564Z',
+      dateTo: '2015-01-31T16:43:51.822Z',
+      currency: '€',
+      price: 66.6666666666
+    }
+  ];
+
   beforeEach(() => {
     angular.module('cheapFlightsApp', ['ng', 'ngMock'])
       .component('flightsList', FlightsListComponent);
@@ -20,7 +35,7 @@ describe('Component flightsList', () => {
     scope.isLoading = null;
     scope.emptyMessage = null;
     scope.flights = null;
-    scope.onFlightSelected = angular.noop;
+    scope.callback = angular.noop;
 
     const el = angular.element(`
       <flights-list
@@ -29,34 +44,58 @@ describe('Component flightsList', () => {
         is-loading="isLoading"
         empty-message="emptyMessage"
         flights="flights"
-        on-flight-selected="onFlightSelected"
+        on-flight-selected="callback(flight)"
       ></flights-list>
     `);
     componentEl = $compile(el)(scope);
     scope.$digest();
   }));
 
-  it('should display provided airports names', () => {
-    expect(false).to.equal(true);
-  });
-
   it('should display loading message based on is-loading', () => {
-    expect(false).to.equal(true);
-  });
-
-  it('should display provided empty message when zero results', () => {
-    expect(false).to.equal(true);
-  });
-
-  it('should display fallback empty message if no custom provided', () => {
-    expect(false).to.equal(true);
+    expect(componentEl[0].querySelector('.flights-list__loading')).to.equal(null);
+    scope.isLoading = true;
+    scope.$apply();
+    expect(componentEl[0].querySelector('.flights-list__loading')).not.to.equal(null);
   });
 
   it('should display provided flights list', () => {
-    expect(false).to.equal(true);
+    expect(componentEl[0].querySelectorAll('.flights-list__item').length).to.equal(0);
+    scope.flights = mockFlights;
+    scope.$apply();
+    expect(componentEl[0].querySelectorAll('.flights-list__item').length).to.equal(mockFlights.length);
+  });
+
+  it('should display provided airports names in results', () => {
+    scope.sourceName = 'Warsaw';
+    scope.destinationName = 'Berlin';
+    scope.flights = mockFlights;
+    scope.$apply();
+    expect(
+      componentEl[0].querySelector('.flights-list__airport--liftoff .flights-list__airport-name').innerText
+    ).to.equal('Warsaw');
+    expect(
+      componentEl[0].querySelector('.flights-list__airport--landing .flights-list__airport-name').innerText
+    ).to.equal('Berlin');
+  });
+
+  it('should display provided empty message when zero results', () => {
+    scope.emptyMessage = 'Nada';
+    scope.flights = [];
+    scope.$apply();
+    expect(componentEl[0].querySelector('.flights-list__item--empty-message').innerText).to.equal('Nada');
+  });
+
+  it('should display fallback empty message if no custom provided', () => {
+    scope.flights = [];
+    scope.$apply();
+    expect(componentEl[0].querySelector('.flights-list__item--empty-message').innerText).to.equal('No results');
   });
 
   it('should call provided callback with selected flight', () => {
-    expect(false).to.equal(true);
+    const ctrl = componentEl.controller('flights-list');
+    scope.$apply();
+    sinon.spy(scope, 'callback');
+    ctrl.onFlightSelected();
+    expect(scope.callback.called).to.equal(true);
   });
 });
